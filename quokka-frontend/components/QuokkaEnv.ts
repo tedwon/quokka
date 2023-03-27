7ec7859e9bfa65a4993a3de467e8ff22f864a51f
Ted Won <jwon@redhat.com>
2023-03-27 23:01:02 +1000

    Thinking how to manage better backend server URL

--- quokka-frontend/components/QuokkaEnv.ts
+++ quokka-frontend/components/QuokkaEnv.ts
@@ -1,10 +1,10 @@
 import {styled} from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 
-// export const BACKEND_SERVER_URL = "http://quokka:8080/quokka";
-// export const BACKEND_SERVER_URL = "http://quokka-backend:8080/quokka";
-export const BACKEND_SERVER_URL = "http://172.16.11.129:8080/quokka/api/v1";
-// export const BACKEND_SERVER_URL = "http://127.0.0.1:8080/quokka";
+// export const BACKEND_HOST = "localhost";
+export const BACKEND_HOST = "172.16.11.129";
+export const BACKEND_SERVER_URL = "http://" + BACKEND_HOST + ":8080/quokka/api/v1";
+// export const BACKEND_SERVER_URL = "https://" + BACKEND_HOST + ":8443/quokka/api/v1";
 
 export const scrollToTop = () => {
     window.scrollTo({
